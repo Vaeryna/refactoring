66230b3c5ebdd6c314dbe12fd12f7ed27508a6e6
Mélanie Leblond <33549336+Vaeryna@users.noreply.github.com>
2026-06-20 17:14:20 +0200

    generate text report

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,48 @@
-import {parseCustomer} from "./services/customer.service.ts";
+import {parseOrders} from "./services/parsing/orders.parser.ts";
+import {parseCustomer} from "./services/parsing/customers.parser.ts";
+import {parseProducts} from "./services/parsing/products.parser.ts";
+import {parseShippingZone} from "./services/parsing/shippingZones.parser.ts";
+import {parsePromotion} from "./services/parsing/promotions.parser.ts";
+import {totalsByCustomers} from "./services/customer-summary.service.ts";
+import {generateReport} from "./services/generateReport.service.ts";
 import path from "path";
+import fs from "fs";
 
 function generateOrderReport() {
-    const filesFolder = path.join(process.cwd(), 'legacy', 'legacy', 'data');
 
-    const custPath = path.join(filesFolder, 'customers.csv');
-    const ordPath = path.join(filesFolder, 'orders.csv');
-    const prodPath = path.join(filesFolder, 'products.csv');
-    const shipPath = path.join(filesFolder, 'shipping_zones.csv');
-    const promoPath = path.join(filesFolder, 'promotions.csv');
+    const customers = parseCustomer('customers.csv');
+    const products = parseProducts('products.csv')
+    const shippingZones = parseShippingZone('shipping_zones.csv')
+    const promotions = parsePromotion('promotions.csv')
+    const orders = parseOrders('orders.csv')
 
-// parseCustomer(custPath)
+    const summaries = totalsByCustomers(orders, promotions)
 
+    const report = generateReport(
+        summaries,
+        customers,
+        orders,
+        products,
+        shippingZones
+    );
 
-    console.log(parseCustomer(custPath))
+    const reportFolder = path.join(process.cwd(), "Report");
 
+    /**
+     * Génération du rapport en format texte dans le dossier "Report"
+     */
+
+    if (!fs.existsSync(reportFolder)) {
+        fs.mkdirSync(reportFolder, {recursive: true});
+    }
+
+    fs.writeFileSync(
+        path.join(reportFolder, "report.txt"),
+        report,
+        "utf-8"
+    );
+
+    console.log("Report generated successfully");
 
 }
 
