dbfba41cb007e5a49721e9fda859ade6cdf32a33
Mélanie Leblond <33549336+Vaeryna@users.noreply.github.com>
2026-06-20 15:50:35 +0200

    discount threshold global constant

--- src/global.constants.ts
+++ src/global.constants.ts
@@ -10,3 +10,7 @@ export const LOYALTY_RATIO = 0.01;
 export const HANDLING_FEE = 2.5;
 export const MAX_DISCOUNT = 200;
 export const MORNING_DISCOUNT_RATE = 0.03;
+export const DISCOUNT_THRESHOLD_1 = 0.05;
+export const DISCOUNT_THRESHOLD_2 = 0.10;
+export const DISCOUNT_THRESHOLD_3 = 0.15;
+export const DISCOUNT_THRESHOLD_PREMIUM = 0.20;
